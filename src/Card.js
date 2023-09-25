2df02d8ac00d1c9575f2b545e788314371743980
nivancheva <nedyalka.tsintsarova@gmail.com>
2023-09-25 23:47:48 +0300

    finish styling

--- src/Card.js
+++ src/Card.js
@@ -3,9 +3,11 @@ import "./Card.css";
 export default function Card({icon, title, text, color}) {
     return (
         <div className={`card color-${color}`} >
-            <img src={`images/${icon}`} alt={title}/>
-            <h2 className="title">{title}</h2>
-            <p>{text}</p>
+            <div className="content">
+                <img src={`images/${icon}`} alt={title}/>
+                <h2 className="title">{title}</h2>
+                <p>{text}</p>
+            </div>
             <button className="button">Learn More</button>
         </div>
     )
